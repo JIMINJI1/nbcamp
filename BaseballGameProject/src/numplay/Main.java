6571cb9fabf95f842d5117525c80be22f17e25ef
JIMINJI1 <mj013031@gmail.com>
2024-09-19 17:41:56 +0900

    Lv2 입력 및 출력 개선

--- BaseballGameProject/src/numplay/Main.java
+++ BaseballGameProject/src/numplay/Main.java
@@ -2,13 +2,13 @@ package numplay;
 
 public class Main {
     public static void main(String[] args) {
-        // InputNumbers 객체 생성
+        // InputNumbers 객체 선언 및 생성
         InputNumbers inputNumbers = new InputNumbers();
         // 사용자 옵션 입력
         inputNumbers.inputNumbers();
 
         //게임시작
+        //NumBaseballGame 객체 선언 및 생성
         NumBaseballGame numBaseballGame = new NumBaseballGame();
-        numBaseballGame.gameLogic();
     }
 }
